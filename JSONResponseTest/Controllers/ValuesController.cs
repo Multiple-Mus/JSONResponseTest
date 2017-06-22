7e363585c282798af2fbfff8926ec6276e6ae62d
Seamus O'Higgins <s.o.higgins1986@gmail.com>
2017-06-22 19:11:42 +0100

    TestUpload

--- JSONResponseTest/Controllers/ValuesController.cs
+++ JSONResponseTest/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ZeroFormatter;
+using ZeroFormatter.Formatters;
 
 namespace JSONResponseTest.Controllers
 {
@@ -13,6 +15,31 @@ namespace JSONResponseTest.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            var p1 = new YogaPose()
+            {
+                PoseId = 123456789,
+                PoseName = "pose1"
+            };
+            var p2 = new YogaPose()
+            {
+                PoseId = 123456799,
+                PoseName = "pose2"
+            };
+            var p3 = new YogaPose()
+            {
+                PoseId = 12346799,
+                PoseName = "pose3"
+            };
+            List<YogaPose> yogList = new List<YogaPose>();
+            yogList.Add(p1);
+            yogList.Add(p2);
+            yogList.Add(p3);
+
+            var bytes = ZeroFormatterSerializer.Serialize(yogList);
+
+            var myJson = System.Text.Encoding.UTF8.GetString(bytes);
+
+
             return new string[] { "value1", "value2" };
         }
 
@@ -41,4 +68,13 @@ namespace JSONResponseTest.Controllers
         {
         }
     }
+
+    [ZeroFormattable]
+    public class YogaPose
+    {
+        [Index(0)]
+        public virtual long PoseId { get; set; }
+        [Index(1)]
+        public virtual string PoseName { get; set; }
+    } 
 }
